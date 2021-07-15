d2ad237a2ec073a2e74fb287792765b1b04e9423
Kabza Ngwana <k.cheese.b@gmail.com>
2021-07-15 13:55:51 +0200

    Done with Services

--- src/app/models/inventory.ts
+++ src/app/models/inventory.ts
@@ -1,4 +1,5 @@
 export interface Inventory {
+    id: number;
     CategoryName: string;
     Description: string;
     Quantity: number;
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,10 +1,78 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Inventory } from '../models/inventory';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  headerOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Inventory[]>{
+    return this.http.get<Inventory[]>(this.baseUrl, this.headerOptions).pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  getById(id: number): Observable<Inventory> {
+    const url = `${this.baseUrl}/{id}`;
+    return this.http.get<Inventory>(url, this.headerOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  create(inventory: Inventory): Observable<Inventory> {
+    const url = `${this.baseUrl}/create}`
+    // return this.http.post<Inventory>(this.baseUrl + '/create/', inventory, this.headerOptions)
+    return this.http.post<Inventory>(this.baseUrl, inventory, this.headerOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  update(id: number, inventory: Inventory): Observable<Inventory> {
+    return this.http.put<Inventory>(this.baseUrl + '/id/', inventory, this.headerOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  delete(id: number): Observable<Inventory> {
+    const url = `${this.baseUrl}/{id}`;
+    return this.http.delete<Inventory>(url, this.headerOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  search(CategoryName: string): Observable<Inventory[]>{
+    const url = `${this.baseUrl}?CategoryName=${CategoryName}`;
+    return this.http.get<Inventory[]>(url, this.headerOptions)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  errorHandler(error: { error: { message: string; }; status: any; message: any; }) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
 }
